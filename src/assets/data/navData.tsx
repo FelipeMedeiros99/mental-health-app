9244cd5eef85ece82957c0932c5743234de30f4e
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-09 19:05:53 -0300

    feat: navbar images and data created

---
+++ src/assets/data/navData.tsx
@@ -0,0 +1,39 @@
+import comunity from "../images/navBarImages/comunity.png"
+import consultation from "../images/navBarImages/consultation.png"
+import home from "../images/navBarImages/home.png"
+import moodMonitor from "../images/navBarImages/mood-monitor.png"
+import settings from "../images/navBarImages/settings.png"
+import relax from "../images/navBarImages/relax.png"
+
+const data = [
+  {
+    image: home,
+    link: "",
+    title: "Tela inicial",
+  },
+  {
+    image: moodMonitor,
+    link: "",
+    title: "Monitor humor",
+  },
+  {
+    image: relax,
+    link: "",
+    title: "Relaxar",
+  },
+  {
+    image: consultation,
+    link: "",
+    title: "Consultas",
+  },
+  {
+    image: comunity,
+    link: "",
+    title: "Comunidade",
+  },
+  {
+    image: settings,
+    link: "",
+    title: "Anjustes",
+  },
+]
