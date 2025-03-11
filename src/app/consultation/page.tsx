ec3dff3785078f153e74138905a0a73d0cac039c
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-11 12:03:58 -0300

    refactor: useState added

--- src/app/consultation/page.tsx
+++ src/app/consultation/page.tsx
@@ -5,24 +5,24 @@ import { FaWhatsapp } from "react-icons/fa";
 
 import style from "./style.module.css"
 import Title from "@/components/Title";
-import { useRouter } from "next/navigation";
 import Link from "next/link";
 import { useState } from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+
+interface MessageDataInterface{
+  message: string
+}
 
 export default function Consultation(){
-
-  const router = useRouter()
-  const [message, setMessage] = useState("")
+  const [message, setMessage] = useState("");
   const fullMessage = `Olá, gostaria de marcar uma consulta \nMotivo: ${message}`
   const wppLink = `https://wa.me/5598987835523?text=${encodeURIComponent(fullMessage)}`
 
   return(
-    <VStack className={style.containerBox}>
+    <VStack as="form" className={style.containerBox}>
       <Title title="Marque sua consulta"/>
-      <Textarea className={style.textarea} placeholder="Qual o motivo da consulta?" value={message} onChange={(e)=>setMessage(e.target.value)} />
-        
-      <Link href={wppLink} onClick={()=>setMessage('')} target="_blank"><Button>Marcar consulta <FaWhatsapp /></Button></Link>
-
+      <Textarea className={style.textarea} placeholder="Como podemos te ajudar?" required value={message} onChange={(e)=>setMessage(e.target.value)}/>
+      <Link type="submit" href={wppLink} target="_blank"><Button>Marcar consulta <FaWhatsapp /></Button></Link>
     </VStack>
   )
 }
