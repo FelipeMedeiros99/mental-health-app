1163c20b6a03d3af52b913c9d910e75d22189b81
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-10 21:15:10 -0300

    feat: activityBox component created

---
+++ src/components/ActivityBox/index.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { Heading, HStack, Text, VStack, Image } from "@chakra-ui/react";
+import Link from "next/link";
+
+import style from "./style.module.css"
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export interface YouTubeVideoData {
+  url: string;
+  thumbnail_height: number;
+  version: string;
+  author_url: string;
+  thumbnail_url: string;
+  author_name: string;
+  title: string;
+  height: number;
+  provider_url: string;
+  type: string;
+  html: string;
+  thumbnail_width: number;
+  width: number;
+  provider_name: string;
+}
+
+interface ActivityBoxInterface {
+  urls: string[];
+  title: string;
+  subtitle: string;
+}
+
+export default function ActivityBox({ urls, title, subtitle }: ActivityBoxInterface) {
+  const [youtubeMetadata, setYoutubeMetadata] = useState<YouTubeVideoData[]>([])
+  const [isDataLoading, setIsDataLoading] = useState(true)
+  useEffect(() => {
+    (async () => {
+      setIsDataLoading(true)
+      try {
+        const videosData = urls.map(url => axios.get<YouTubeVideoData>(`https://noembed.com/embed?url=${url}`))
+        const responses = await Promise.all(videosData);
+        const result: YouTubeVideoData[] = responses.map(response => response.data);
+        setYoutubeMetadata(result)
+      } catch (e) {
+        alert("Erro ao carregar videos")
+        console.log(e)
+      } finally {
+        setIsDataLoading(false)
+      }
+    })()
+  }, [])
+
+  return (
+    <>
+    { !isDataLoading &&
+    <VStack w="100%" h="">
+      <Heading>{title}</Heading>
+      <Text>{subtitle}</Text>
+      <HStack className={style.containerLinks}>
+        {youtubeMetadata.map((data, index) => (
+          <Link key={index} className={style.boxLink} href={data.url} target="_blank">
+            <VStack className={style.imageBox}>
+              <Image className={style.image} src={data.thumbnail_url} alt={data.title} />
+            </VStack>
+            <Text padding="0 1rem 0 1rem">{data.title.substring(0, 1).toLocaleUpperCase()}{data.title.substring(1, 40).toLocaleLowerCase()}...</Text>
+          </Link>
+        ))}
+      </HStack>
+    </VStack>
+  }
+  </>
+  )
+}
