a09f7343c3ce7dea445aa9ad7e6945c5f1b1ef49
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-11 14:21:22 -0300

    refactor: ternario added at message

--- src/app/consultation/page.tsx
+++ src/app/consultation/page.tsx
@@ -6,7 +6,7 @@ import { FaWhatsapp } from "react-icons/fa";
 import style from "./style.module.css"
 import Title from "@/components/Title";
 import Link from "next/link";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 interface MessageDataInterface{
@@ -15,13 +15,16 @@ interface MessageDataInterface{
 
 export default function Consultation(){
   const [message, setMessage] = useState("");
-  const fullMessage = `Olá, gostaria de marcar uma consulta \nMotivo: ${message}`
-  const wppLink = `https://wa.me/5598987835523?text=${encodeURIComponent(fullMessage)}`
+  
+  const wppLink = useMemo(()=>{
+    const fullMessage =message?`Olá, gostaria de marcar uma consulta! \nMotivo: ${message}`:"Olá, gostaria de marcar uma consulta!"
+    return `https://wa.me/5598987835523?text=${encodeURIComponent(fullMessage)}`
+  }, [message])
 
   return(
-    <VStack as="form" className={style.containerBox}>
+    <VStack className={style.containerBox}>
       <Title title="Marque sua consulta"/>
-      <Textarea className={style.textarea} placeholder="Como podemos te ajudar?" required value={message} onChange={(e)=>setMessage(e.target.value)}/>
+      <Textarea className={style.textarea} placeholder="Como podemos te ajudar?" value={message} onChange={(e)=>setMessage(e.target.value)}/>
       <Link type="submit" href={wppLink} target="_blank"><Button>Marcar consulta <FaWhatsapp /></Button></Link>
     </VStack>
   )
