109c1f3f1fb3c3bc4c38f844514bc2254b329307
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-11 14:59:24 -0300

    feat: zindex added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import { Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 
@@ -8,8 +10,8 @@ import style from "./style.module.css"
 export default function Page() {
   return (
     <VStack h="100%" w="100%" position="fixed" top="0" >
-      <Image className={style.image} src={Logo} alt="Logo do app" />
-      <VStack position="absolute" top="50%" translate="0 -50%" color="white" w="100%" maxW="30rem" padding="0.7rem" textAlign="center">
+      <Image className={style.image} src={Logo} alt="Logo do app" /> 
+      <VStack position="absolute" top="50%" translate="0 -50%" color="white" w="100%" maxW="30rem" padding="0.7rem" textAlign="center" zIndex="2">
         <Text fontSize="2rem" fontWeight="bold">
           Olá, [Fulano]! 👋
         </Text>
