e39f7f98764ef61c73804e7cbe817311caa9fb58
Felipe Medeiros <felipe.m.c.b.fm@gmail.com>
2025-03-14 16:38:29 -0300

    feat: google and facebook login added

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 
-import { Button, Field, Input, Text, VStack } from "@chakra-ui/react";
+import { Button, Field, HStack, Input, Text, VStack } from "@chakra-ui/react";
 import Image from "next/image";
 import { SubmitHandler, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 
 import ContainerBox from "@/components/ContainerBox";
 import Footer from "@/components/Footer";
 import Title from "@/components/Title";
 import icone from "@/assets/images/logo.png"
-import { useRouter } from "next/navigation";
+import facebook from "@/assets/images/loginImages/facebook.png"
+import google from "@/assets/images/loginImages/google.png"
+
 
 interface Name{
   name: string
@@ -50,15 +53,17 @@ export default function SignIn() {
 
 
   return (
-    <VStack h="100%" w="100%" padding="1rem" bgColor="white">
-      <Image src={icone} alt="Icone app" style={{width: "auto", maxWidth: "18rem", objectFit: "cover"}}/>
-      <ContainerBox zIndex="2">
+    <VStack h="100%" w="100%" padding="1rem">
+      <Image src={icone} alt="Icone app" style={{width: "10rem", maxWidth: "100%", objectFit: "cover"}}/>
+
         <Title
           textAlign="center"
-          title="Seja muito bem-vindo! 😊"
-          subtitle="Estamos felizes em te receber. Como podemos te chamar?"
+          title="Seja bem-vindo! 😊"
+          subtitle="Efetue seu login"
           alignItems="center"
+          fontSize="1rem"
         />
+
         <VStack as="form" w="100%" alignItems="flex-start" onSubmit={handleSubmit(onSubmit)}>
           <Field.Root>
             <Field.Label>
@@ -68,7 +73,12 @@ export default function SignIn() {
           </Field.Root>
           <Button type="submit" w="100%">Entrar</Button>
         </VStack>
-      </ContainerBox>
+
+        <HStack w="10rem" justifyContent="space-between" marginTop="3rem">
+          <Image src={facebook} alt="login com o facebook" style={{width: "2rem", padding: "0.2rem", backgroundColor: "white", borderRadius: "5rem"}}/>
+          <Image src={google} alt="login com o gmail" style={{width: "2rem", padding: "0.2rem", backgroundColor: "white", borderRadius: "5rem"}}/>
+        </HStack>
+
       <Footer />
     </VStack>
   );
